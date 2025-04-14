b059ef00278d9f0fa1fc4ccd12205a9bf111d875
lcy <lizichengziyezi@gmail.com>
2025-04-14 15:43:57 +0800

    初始化提交

---
+++ src/main/java/com/example/comeonclass/ComeonclassApplication.java
@@ -0,0 +1,13 @@
+package com.example.comeonclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeonclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeonclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/comeonclass/ComeonclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.comeonclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeonclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
